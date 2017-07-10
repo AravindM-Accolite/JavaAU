ead90d2dd005470888b9be4f0acdca691449f799
AravindM-Accolite <aravindkumar.metku@github.com>
2017-07-10 20:11:10 +0530

    Test Class added auto doc and performed converge

---
+++ Test.java
@@ -0,0 +1,23 @@
+package com.au.sample;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Test.
+ */
+public class Test {
+	
+	/** The debug. */
+	private static boolean DEBUG = true;
+	
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
+	public static void main(String[] args) {
+		for(int i=0;i<10; i++){
+			i++;
+		}
+		System.out.println("Hello World");
+	}
+}
